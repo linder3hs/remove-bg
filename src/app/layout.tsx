4fe79c36fd693e3015a03bb5eefc60e7757f4ba3
linder3hs <linderhassinger00@gmail.com>
2023-09-10 22:16:19 -0500

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Remove Background',
-  description: 'Remove Background from images',
-}
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Remove background from images" />
+        <meta name="author" content="Linder Hassinger" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Remove background from images</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
